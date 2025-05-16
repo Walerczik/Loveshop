3c53e663f0db27c229375b9507913d54af147fdc
Walerczik <xomathebest1@gmail.com>
2025-05-16 13:57:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ADMIN_ID = os.getenv("ADMIN_ID")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBAPP_HOST = os.getenv("WEBAPP_HOST", "0.0.0.0")
 WEBHOOK_PATH = "/webhook"
