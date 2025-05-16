0f647ca5e14e3dbd70a389f1e0c0f6a0af3e4ef7
Walerczik <xomathebest1@gmail.com>
2025-05-16 13:26:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import register_handlers
 
@@ -6,8 +6,3 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 register_handlers(dp)
-
-if __name__ == '__main__':
-    from models import load_data  # чтобы создать файл при первом запуске
-    load_data()
-    executor.start_polling(dp, skip_updates=True)
