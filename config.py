03560b54621476b79ecda1fc5443ccdd2d9c658c
Walerczik <xomathebest1@gmail.com>
2025-05-16 19:22:38 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv("API_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+WEBHOOK_PATH = "/webhook"
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+GIRL_ID = int(os.getenv("GIRL_ID"))
+
+db_path = "data/db.json"
+
+def load_db():
+    if not os.path.exists(db_path):
+        return {"balances": {}, "products": {}}
+    with open(db_path, "r") as f:
+        return json.load(f)
+
+def save_db(data):
+    with open(db_path, "w") as f:
+        json.dump(data, f, indent=2)
