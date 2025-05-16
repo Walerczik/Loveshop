09617db156442c34367d877ac162d0047092535a
Walerczik <xomathebest1@gmail.com>
2025-05-16 14:24:41 +0200

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
-from handlers import register_handlers
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-
-register_handlers(dp)
