2ffabcb51bd1782d819fcca7bd677bac008b10e7
Walerczik <xomathebest1@gmail.com>
2025-05-16 14:26:19 +0200

    Create webhook_server.py

---
+++ webhook_server.py
@@ -0,0 +1,36 @@
+import logging
+from aiohttp import web
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import Update
+
+from config import BOT_TOKEN, WEBHOOK_URL, WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_PATH
+from handlers import register_handlers
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+register_handlers(dp)
+
+async def on_startup(app):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(app):
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+async def handle(request):
+    request_data = await request.json()
+    update = Update.to_object(request_data)
+    await dp.process_update(update)
+    return web.Response()
+
+app = web.Application()
+app.router.add_post(WEBHOOK_PATH, handle)
+app.on_startup.append(on_startup)
+app.on_shutdown.append(on_shutdown)
+
+if __name__ == '__main__':
+    web.run_app(app, host=WEBAPP_HOST, port=int(WEBAPP_PORT))
