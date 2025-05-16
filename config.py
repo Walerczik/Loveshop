5eebec26a098c3b31870691c6ffc7f06f7d57f26
Walerczik <xomathebest1@gmail.com>
2025-05-16 13:00:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # ВАЖНО: здесь ключ — это строка-переменная, а не сам ID
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # по умолчанию 0
+USER_ID = int(os.getenv("USER_ID", "0"))
