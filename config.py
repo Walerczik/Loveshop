de27fa16f88a458e52c59726a0cfc2b9f81991b0
Walerczik <xomathebest1@gmail.com>
2025-05-16 12:20:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("7059973966:AAGgfFCEFTkSU37XadpRivPaYKsb3YaARVI")
-ADMIN_ID = int(os.getenv("6909254042"))  # Твой Telegram ID
-USER_ID = int(os.getenv("436851363"))    # Telegram ID девушки
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # Твой Telegram ID
+USER_ID = int(os.getenv("USER_ID"))    # Telegram ID девушки
